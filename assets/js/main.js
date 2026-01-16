621a08cf913d634048d31a8ff44a987860f8485c
Alejandro <alejandro.ruiz.santana@ieslaarboleda.es>
2026-01-16 22:16:45 +0100

    Update portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,3 +59,8 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.section-title',{delay: 100});
+sr.reveal('.estudios__data',{interval: 100});
+sr.reveal('.experiencia__data',{delay: 200});
+sr.reveal('.proyectos__data',{delay: 200});
+sr.reveal('.about__container',{delay: 300}); 
